1e564cb52d9d2fa0ff21d2037c445ff71c8a7001
Lad Jankovic <ljankovic@yahoo.com>
2022-04-23 21:40:29 -0400

    Popular picks with images

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -15,15 +15,24 @@ export default function Popular() {
       );
     const data = await api.json();    
     setPopular(data.recipes);
+    console.log(data.recipes);
   };
 
   return (
     <div>
       {popular.map((recipe) => {
         return(
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
+          <Wrapper>
+            <h3>Popular Picks</h3>
+            {popular.map((recipe) => {
+              return(
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              );
+            })}
+          </Wrapper>
         );
       })}
     </div>
@@ -31,5 +40,11 @@ export default function Popular() {
 }
 
 const Wrapper=styled.div`
-  margin: 4rem 0rem
+  margin: 4rem 0rem;
 `;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+
+`
