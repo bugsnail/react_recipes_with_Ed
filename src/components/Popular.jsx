0eefb6af927d8c846666dc9d640af3d06226fb6b
Lad Jankovic <ljankovic@yahoo.com>
2022-04-23 22:21:04 -0400

    Local storage

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,13 +12,23 @@ export default function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`
-      );
-    const data = await api.json();    
-    setPopular(data.recipes);
-    console.log(data.recipes);
-  };
+
+    const check = localStorage.getItem('popular')
+
+    if (check){
+      setPopular(JSON.parse(check));
+    }else{
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`
+        );
+        const data = await api.json();    
+
+        localStorage.setItem('popular', JSON.stringify(data.recipes));
+
+        setPopular(data.recipes);
+        console.log(data.recipes);
+      }
+};
 
   return (
     <div>
@@ -35,10 +45,11 @@ export default function Popular() {
 
         {popular.map((recipe) => {
           return(
-            <SplideSlide>
+            <SplideSlide key={recipe.id}>
               <Card>
                 <p>{recipe.title}</p>
                 <img src={recipe.image} alt={recipe.title} />
+                <Gradient />
               </Card>
             </SplideSlide>
           );
@@ -46,7 +57,7 @@ export default function Popular() {
         </Splide>
       </Wrapper>
     </div>
-  )
+  );
 }
 
 const Wrapper=styled.div`
@@ -57,8 +68,39 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img{
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backgroun: liner-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+`
